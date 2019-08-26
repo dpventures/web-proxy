7a4d7de91c63cf476f78313df347f21abe297d78
dpventures <47573247+dpventures@users.noreply.github.com>
2019-08-25 23:50:59 -0500

    add proxy logic

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
 
 const port = process.env.PORT || 5300;
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.listen(port);
 console.log(`starting app at http://localhost:${port}`);
 
 app.get('/test', (req, res) => {
   res.send('api is working!');
+});
+
+app.post('/geturl', (req, res) => {
+  const url = req.body.url;
+  request(url, function (error, response, body) {
+    if (error) return res.sendStatus(500);
+    if (response && response.statusCode != 200) return res.sendStatus(response.statusCode);
+    res.send(body);
+  });
 });
